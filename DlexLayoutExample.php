f011364c4364437cdd60da80a06d0a3628a2d6ff
James A. Brannan <jamesabrannan@hushmail.com>
2021-11-18 13:55:02 -0500

    Update DlexLayoutExample.php

--- DlexLayoutExample.php
+++ DlexLayoutExample.php
@@ -8,8 +8,8 @@ class DlexLayoutExample
     private static string $BasePath = __DIR__;
     public static function RunExample()
     {
-        $layoutData = new LayoutDataResource(DlexLayoutExample::$BasePath . "/Resources/client-libraries-examples/AllReportElementsData.json");
-        $dlexEndpoint = new DlexLayout("samples/shared/dlex/AllReportElements.dlex", $layoutData);
+        $layoutData = new LayoutDataResource(DlexLayoutExample::$BasePath . "/Resources/client-libraries-examples/SimpleReportData.json");
+        $dlexEndpoint = new DlexLayout("samples/shared/dlex/SimpleReportWithCoverPage.dlex", $layoutData);
         $response = $dlexEndpoint->Process();
         file_put_contents(DlexLayoutExample::$BasePath . "./output/dlex-output.pdf", $response->Content);
         echo ("PDF Received: " . DlexLayoutExample::$BasePath . "./output/dlex-output.pdf");
