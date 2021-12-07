dfe4e33ff6f4e82cda59e8124388c6614d3612e2
James A. Brannan <jamesabrannan@hushmail.com>
2021-12-06 22:45:12 -0500

    Update GettingStartedInFive.php

--- GettingStartedInFive.php
+++ GettingStartedInFive.php
@@ -12,10 +12,9 @@ class GettingStartedInFive
     {
         $layoutData = new LayoutDataResource(GettingStartedInFive::$BasePath . "getting-started.json");
         $dlexEndpoint = new DlexLayout("samples/getting-started/getting-started.dlex", $layoutData);
-        $dlexEndpoint->ApiKey = "DP.poEtD7F5xxxxtD1Ulp3qPcolUFaCcQFxWOvuNUqm/WragUdOSaAesnu3L6XE";
+        $dlexEndpoint->ApiKey = "DP.poEtD7F5tD1Ulp3qPcolUFaCcQFxWOvuNUqm/WragUdOSaAesnu3L6XE";
         $response = $dlexEndpoint->Process();
         file_put_contents(GettingStartedInFive::$BasePath . "getting-started-output.pdf", $response->Content);
     }
 }
-
 GettingStartedInFive::Run();
